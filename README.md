0ae9958e019ef18a9f1970df7fa2050619a18bcc
Joseph Cordell <joseph@jcordell.com>
2021-03-06 10:07:27 -0800

    Updated timer/score system, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # CodingQuiz
+
+Created a quiz on HTML using javascript
+
+## Project Links
+
+[GitHub Repository](https://github.com/JosephCordell/CodingQuiz)
+
+[Webpage](https://josephcordell.github.io/CodingQuiz/)
+
+## Screenshot of webpage
+![Screenshot of webpage.](./assets/images/codingQuiz.png)
+
+## Features
+- A quiz with 10 randomized questions
+- A countdown timer that starts at 60 seconds
+- High Score page with a reset button
+
+## What I learned
+
+- How to hide elements
+- Creating a timer that reacts to elements on the page
+- How to utilize local storage
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,32 +3,99 @@ const questionContainerEl = document.getElementById('questionContainer')
 const questionEl = document.getElementById('question')
 const answerButtonEl = document.getElementById('answerButtons')
 const questionNumber = document.getElementById('questionNumber')
-const highScores = document.getElementById('highScores')
 const quizScore = document.getElementById('quizScore')
 const result = document.getElementById('correct')
 const submitScoreEl = document.getElementById('submitScore')
 const scoreNameInput = document.querySelector('#scoreName')
+const home = document.getElementById('title')
+const highScores = document.getElementById('highScores')
+const submission = document.getElementById('submission')
+const clearScoresEl = document.getElementById('clearScore') 
+const highScoresEl = document.getElementById('high')
 
+home.addEventListener('click', goHome)
 start.addEventListener('click', startGame)
 submitScoreEl.addEventListener('click', submitScore)
+highScoresEl.addEventListener('click', highscorePage)
 
 let score = 0
-let shuffledQuestions, CurrentQuestionIndex
+let timer = 60
+let shuffledQuestions, CurrentQuestionIndex, gameTime, timeRemaining
 
+//lets user go back to the homepage of the quiz
+function goHome() {
+    start.classList.remove('hide')
+    questionContainerEl.classList.add('hide')
+    highScores.classList.add('hide')
+    submission.classList.add('hide')
+    quizTimer(false)
+    document.getElementById('timer').innerText = timer
+    score = 0
+}
+
+//brings user to the highscore page
+function highscorePage() {
+    start.classList.add('hide')
+    quizScore.classList.add('hide')
+    questionContainerEl.classList.add('hide')
+    highScores.classList.remove('hide')
+    submission.classList.add('hide')
+    quizTimer(false)
+    document.getElementById('timer').innerText = timer
+    printHighscores()
+    score = 0
+}
+
+//clears high score
+clearScoresEl.addEventListener('click',() => {
+    localStorage.removeItem('highscoreList')
+    printHighscores()
+}, false)
+
+//sets up the HTML for 
 function startGame() {
+    timeRemaining = timer
     score = 0
     start.classList.add('hide')
     questionContainerEl.classList.remove('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     CurrentQuestionIndex = 0
+    quizTimer(true)
     nextQuestion()
+
 }
 
+//runs the timer for the quiz
+const quizTimer = (enable, penalty = 0) => {
+    clearInterval(gameTime)
+    if (enable) {
+        timeRemaining = timeRemaining - penalty
+        if ( timeRemaining> 0 ){
+            document.getElementById('timer').innerText = timeRemaining
+            gameTime = setInterval( () => {
+                timeRemaining -= 1
+                document.getElementById('timer').innerText = timeRemaining
+                if (timeRemaining <= 0 ) {
+                    document.getElementById('timer').innerText = '0'
+                    clearInterval(gameTime)
+                    quizOver()
+                }
+            }, 1000)
+        } else {
+            document.getElementById('timer').innerText = '0'
+            quizOver()
+        } 
+
+    }
+}
+
+//loads next question for the quiz
 function nextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[CurrentQuestionIndex])
 }
 
+//brings up the next question in the list and displays the answers
 function showQuestion(question) {
     questionNumber.textContent = `Question ${CurrentQuestionIndex+1}`
     questionEl.innerText = question.question
@@ -45,92 +112,101 @@ function showQuestion(question) {
 
 }
 
+//clears the answer pool
 function resetState() {
     while (answerButtonEl.firstChild) {
         answerButtonEl.removeChild(answerButtonEl.firstChild)
     }
 }
 
+//checks to see if the correct answer is chosen and tells the user if they are correct or incorrect
 function selectAnswer(e){
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct
-    console.log(correct)
     if (correct) {
         score++;
         result.textContent = "Correct!"
         result.classList.remove('hide')
+        setTimeout(() => {
+            result.classList.add('hide')
+        }, 1000); 
      } else {
         result.textContent = "Incorrect"
+        quizTimer(true, 5)
         result.classList.remove('hide')
+        setTimeout(() => {
+            result.classList.add('hide')
+        }, 1000); 
      }
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonEl.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-      })
     if (shuffledQuestions.length > CurrentQuestionIndex +1) {
         CurrentQuestionIndex++
         nextQuestion()
     } else {
         result.classList.add('hide')
+        quizTimer(false)
         quizOver()
     }
 
 }
 
+//brings user to the high score page
 function quizOver() {
     questionContainerEl.classList.add('hide')
     quizScore.textContent = `You have a score of ${score}`
     highScores.classList.remove('hide')
-
+    quizScore.classList.remove('hide')
+    printHighscores()
+    if (score > 0){
+    submission.classList.remove('hide')
+    }
 }
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct){
-        element.classList.add('correct')
+
+//lets user submit their high score
+function submitScore () {
+    userName = scoreNameInput.value.trim()
+
+    if (userName && score > 0) {
+
+        let scoreTable = JSON.parse(localStorage.getItem('highscoreList')) || []
+        
+        let finalScore = {
+            name: userName,
+            score
+        }
+        scoreTable.push(finalScore)
+        scoreTable.sort(function(val1, val2){
+            return val2.score - val1.score;
+        })
+
+        scoreTable = scoreTable.filter(function(score, index) {
+            return index < 10;
+        })
+        localStorage.setItem("highscoreList", JSON.stringify(scoreTable))
+    }
+    quizScore.classList.add('hide')
+    submission.classList.add('hide')
+    printHighscores()
+}
+
+//prints the high scores on the high score page
+function printHighscores() {
+    let scoreTable = JSON.parse(localStorage.getItem('highscoreList')) || []
+    if (scoreTable.length > 0 ){
+    tableString = '<table><tr><th>Rank</th><th>Name</th><th>Score</th></tr>'
+    for (var i = 0; i < scoreTable.length; i++) {
+        tableString += `<tr><td>${i+1}</td><td>${scoreTable[i].name}</td><td>${scoreTable[i].score}</td></tr>`
+    }
+
+    document.getElementById('records').innerHTML =  tableString + '</table>' 
     } else {
-        element.classList.add('wrong')
+        document.getElementById('records').textContent =  'No Scores Found'
     }
-}
-
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-}
-
-function submitScore (event) {
-    event.preventDefault();
-    let user = {
-        scoreName: scoreNameInput.value.trim(),
-        score: score
-    }
-
-    localStorage.setItem('user', JSON.stringify(user))
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ }
 
 
 const questions = [
     {
-        question:`What does HTML stand for?`,
+        question:`What does HTML stand for?1`,
         answers: [
             { text: `Home Tool Markup Language`, correct: false},
             { text: `Hyper Text Markup Language`, correct: true},
@@ -139,48 +215,84 @@ const questions = [
         ]
     },
     {
-        question:`Who is making the Web standards?`,
+        question:`Who is making the Web standards?2`,
         answers: [
-            { text: `Google`, correct: true},
+            { text: `Google`, correct: false},
             { text: `Microsoft`, correct: false},
             { text: `Brian Duimstra`, correct: false},
-            { text: `The World Wide Web Consortium`, correct: false}
+            { text: `The World Wide Web Consortium`, correct: true}
         ]
     },
     {
-        question:`How can you open a link in a new tab/browser window?`,
+        question:`How can you open a link in a new tab/browser window`,
         answers: [
-            { text: `<a href = "url"new>`, correct: true},
+            { text: `<a href = "url"new>`, correct: false},
             { text: `<a href = "url" target="new">`, correct: false},
-            { text: `<a href="url" target="_blank">`, correct: false},
+            { text: `<a href="url" target="_blank">`, correct: true},
             { text: `<a href="url" target="Bonkers"`, correct: false}
         ]
     },
     {
-        question:`Who is making the Web standards?`,
+        question:`Choose the correct HTML element for the largest heading:`,
         answers: [
-            { text: `Google`, correct: true},
-            { text: `Microsoft`, correct: false},
-            { text: `Brian Duimstra`, correct: false},
-            { text: `The World Wide Web Consortium`, correct: false}
+            { text: `<h1>`, correct: true},
+            { text: `<heading>`, correct: false},
+            { text: `<h6>`, correct: false},
+            { text: `<head>`, correct: false}
         ]
     },
     {
-        question:`Who is making the Web standards?`,
+        question:`How can you make a numbered list?`,
         answers: [
-            { text: `Google`, correct: true},
-            { text: `Microsoft`, correct: false},
-            { text: `Brian Duimstra`, correct: false},
-            { text: `The World Wide Web Consortium`, correct: false}
+            { text: `<dl>`, correct: false},
+            { text: `<ol>`, correct: true},
+            { text: `<list>`, correct: false},
+            { text: `<ul>`, correct: false}
         ]
     },
     {
-        question:`Who is making the Web standards?`,
+        question:`Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?`,
         answers: [
-            { text: `Google`, correct: true},
-            { text: `Microsoft`, correct: false},
-            { text: `Brian Duimstra`, correct: false},
-            { text: `The World Wide Web Consortium`, correct: false}
+            { text: `londesc`, correct: false},
+            { text: `src`, correct: false},
+            { text: `alt`, correct: true},
+            { text: `title`, correct: false}
+        ]
+    },
+    {
+        question:`Which doctype is correct for HTML5?`,
+        answers: [
+            { text: `<!DOCTYPE HTML PUBLIC" -NOT THIS ANSWER`, correct: false},
+            { text: `<!DOCTYPE HTML5>`, correct: false},
+            { text: `<!DACTYPE html>`, correct: false},
+            { text: `<!DOCTYPE html>`, correct: true}
+        ]
+    },
+    {
+        question:`In HTML, onblur and onfocus are:`,
+        answers: [
+            { text: `Style attributes`, correct: false},
+            { text: `HTML elements`, correct: false},
+            { text: `Event`, correct: true},
+            { text: `Fun Elements`, correct: false}
+        ]
+    },
+    {
+        question:`The HTML <canvas> element is used to:`,
+        answers: [
+            { text: `display database records`, correct: false},
+            { text: `draw graphics`, correct: true},
+            { text: `create draggable elements`, correct: false},
+            { text: `manipulate data in MySQL`, correct: false}
+        ]
+    },
+    {
+        question:`Which input type defines a slider control?`,
+        answers: [
+            { text: `range`, correct: true},
+            { text: `controls`, correct: false},
+            { text: `search`, correct: false},
+            { text: `slider`, correct: false}
         ]
     },
 ]
